b899a8432e7e89c953222f5324663be2a01b1b6f
GhostCrab <Ryan.Pielow@gmail.com>
2022-01-17 17:07:40 -0800

    update

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,18 @@
-// THIS IS A TEST
-
 /** @param {import(".").NS } ns */
 export async function main(ns) {
-    
+    const args = ns.flags([["help", false]]);
+    if (args.help) {
+        ns.tprint("This script will enhance your HUD (Heads up Display) with custom statistics.");
+        ns.tprint(`Usage: run ${ns.getScriptName()}`);
+        ns.tprint("Example:");
+        ns.tprint(`> run ${ns.getScriptName()}`);
+        return;
+    }
 
-    ns.tprintf("%s", writeData)
-    
+    const self = globalThis;
+    const doc = self["document"];
+    const hook0 = doc.getElementById("overview-extra-hook-0");
+
+    let p = doc.createElement("Typography", { id: "overview-extra-hook-X", classes: "{{ root: classes.hack }}" });
+    hook0.after(p)
 }
